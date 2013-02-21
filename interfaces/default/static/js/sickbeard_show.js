040d220677a6a79cbd8080733eddbcce27a56d73
Floris <info@styxit.com>
2013-02-21 16:41:31 +0100

    sicbeard load show modal
    Show modal while loading show details. Show error on failure.

--- interfaces/default/static/js/sickbeard_show.js
+++ interfaces/default/static/js/sickbeard_show.js
@@ -1,9 +1,14 @@
 function loadShowData(showid){
+  blockPage('Loading show details...', '<img src="/img/loader.gif" />');
   $.ajax({
     url: '/json/?which=sickbeard&action=getshow&tvdbid=' + showid,
     type: 'get',
     dataType: 'json',
     success: function(data){
+      if (data.result != 'success') {
+        notifyError('Error', 'The show with id <b>'+showid+'</b> could not be found.');
+        return;
+      }
       data = data.data;
       $('.sickbeard_showname').html(data.show_name);
       $('.sickbeard_status').html(data.status);
@@ -13,6 +18,12 @@ function loadShowData(showid){
       if (data.next_ep_airdate != '') {
         $('.sickbeard_next_air').html('Next: '+sickbeardFormatDate(data.next_ep_airdate));
       }
+    },
+    error: function(){
+      alert('Error while loading show');
+    },
+    complete: function(){
+      unblockPage();
     }
   });
 }
